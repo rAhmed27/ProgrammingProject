19849dfbf5d3f84b7395aaaedb44b72cb3065599
rAhmed27 <24ahmedre@centralfoundationboys.co.uk>
2025-09-08 09:10:24 +0000

    raycasting basic set up

--- rayCasting.py
+++ rayCasting.py
@@ -1,4 +1,23 @@
 import pygame
 import math
 
-res
+res = width, height = 1600, 900
+fps = 60
+playerPOS = 1.5, 5
+playerAngle = 0
+playerSpeed = 0.004
+playerROTspeed = 0.002
+
+class RayCasting:
+    def __init__(self, game):
+        self.game = game
+    
+    def rayCast(self):
+        rayAngle = self.game.player.angle - (0.5 * playerPOV) + 0.0001
+        playerX, playerY =
+        mapX, mapY =
+    
+    def update(self):
+        self.rayCast()
+
+    
